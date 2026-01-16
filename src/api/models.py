a2e71601666f28d886df20acd78cffb5d0c565f2
HRLopezP <hrlp843@gmail.com>
2026-01-16 18:59:18 +0000

    Primeros 2 modelos

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,62 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, DateTime, Text, Enum, ForeignKey, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+from typing import Optional
+from typing import List
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class Rol (db.Model):
+    __tablename__ = 'rol'
+    id_rol: Mapped[int] = mapped_column(primary_key=True)
+    name_rol: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(
+        timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(
+        timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+
+    users: Mapped[List["User"]] = relationship(back_populates="rol")
+
+
+    def __repr__(self):
+        return f'<Rol {self.name_rol}>'
 
     def serialize(self):
         return {
-            "id": self.id,
+            "id": self.id_rol,
+            "name_rol": self.name_rol,
+        }
+
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id_user: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(100), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    profile: Mapped[Optional[str]] = mapped_column(String(255), nullable=True, default=None)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+
+    rol_id: Mapped[int] = mapped_column(ForeignKey('rol.id_rol'), nullable=False)
+    rol: Mapped["Rol"] = relationship(back_populates="users")
+    
+    def __repr__(self):
+        return f'<User {self.mail}>'
+
+    def serialize(self):
+        initials = f"{self.name}+{self.lastname}"
+        return {
+            "id": self.id_user,
+            "name": self.name,
+            "lastname": self.lastname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "rol": self.rol.name_rol if self.rol else None, 
+            "is_active": self.is_active,
+            "image": self.profile if self.profile else f"https://ui-avatars.com/api/?name={initials}&size=128&background=random&rounded=true"
         }
